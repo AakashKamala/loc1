e8c89e2814ff07d293a57a0cfde47ad9d462933c
Aakash <aakashkumar2908@gmail.com>
2025-03-22 23:00:24 +0530

    cas

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require("express")
 const app=express()
 
-app.get("/",(req,res)=>{
-    console.log(req.ip)
-    res.send("Hello World")
-}
-)
+app.get("/", (req, res) => {
+    const ip = req.connection.remoteAddress.replace(/^::ffff:/, ""); // Normalize IPv4-mapped IPv6 addresses
+    console.log(ip);
+    res.send("Hello World");
+});
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
